dfebe694bf94dc4f036b01b61243ba0d3cbaeb55
Alberto Sobrinho <aclsobrinho@hotmail.com>
2019-07-23 12:20:10 -0300

    read the recipient list from csv file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import csv
 from addressing import Sender, Recipient
 
-recipient_set = [Recipient('email_1@email.com', 'name_1'),
-                Recipient('email_2@email.com', 'name_2')]
+with open('recipient_list.csv', encoding='utf-8', newline='') as csvfile:
+    recipient_list = csv.reader(csvfile, delimiter=',')
+    next(recipient_list)
+    recipient_set = [Recipient(recipient[0], recipient[1]) for recipient in recipient_list]
 
 sender = Sender('email_0@email.com', 'name_0')
 
 with open('path_to_text', encoding='utf-8') as txt:
-    content = txt.read()
+    message_content = txt.read()
+message_subject = 'Subject'
 
-subject = 'Subject'
-
-sender.send_email(recipient_set, content, subject)
+sender.send_email(recipient_set, message_content, message_subject)
